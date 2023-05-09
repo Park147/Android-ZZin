3f0a46999bf66e9d11f5afd83f50103b2d37b1ac
Park147 <54125201+Park147@users.noreply.github.com>
2023-05-09 17:49:36 +0900

    저장테스트1

---
+++ app/src/main/java/com/example/androidlab0509_start/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.androidlab0509_start
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
